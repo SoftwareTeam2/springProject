0a80c86362c2c62b704c46f14089faed42bc022f
신현호 <47271188+hyeonho9877@users.noreply.github.com>
2021-05-20 23:39:42 +0900

    예약 취소, 워크인 등록, 워크인 조회, 테이블 변경, 도착등록 까지

--- youngcha/src/main/java/Team2/youngcha/hellospring/repository/WalkInRepository.java
+++ youngcha/src/main/java/Team2/youngcha/hellospring/repository/WalkInRepository.java
@@ -1,9 +1,42 @@
 package Team2.youngcha.hellospring.repository;
 
 import Team2.youngcha.hellospring.domain.WalkIn;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
 @Repository
-public interface WalkInRepository extends JpaRepository<WalkIn,Long> {
+public class WalkInRepository {
+    private final EntityManager em;
+
+    public WalkInRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Long save(WalkIn walkIn) {
+        //validateDuplicateTable(reservation); // 중복 시간대에 예약인지 확인 나중에는 아예 안보이게 하는 것도 가능??
+
+        em.persist(walkIn);
+        return walkIn.getOid();
+    }
+
+    /*
+    private void validateDuplicateTable(Reservation reservation) {
+
+        // 로직
+    }
+    */
+
+    public List<WalkIn> findAll() {
+        return em.createQuery("select w from WalkIn w", WalkIn.class)
+                .getResultList();
+    }
+
+    public List<WalkIn> findByCustomerID(String id) {
+        List<WalkIn> result = em.createQuery("select w from WalkIn w where w.customerId=:customerId",WalkIn.class)
+                .setParameter("customerId", id)
+                .getResultList();
+        return result;
+    }
 }
