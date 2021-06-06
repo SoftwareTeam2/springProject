725d8cb64088d8695aff37a111de53f25e7c8de6
BEOMSEOK <77001461+chunbu@users.noreply.github.com>
2021-06-06 20:53:48 +0900

    Add files via upload

--- youngcha/src/main/java/Team2/youngcha/hellospring/controller/HomeController.java
+++ youngcha/src/main/java/Team2/youngcha/hellospring/controller/HomeController.java
@@ -1,7 +1,12 @@
 package Team2.youngcha.hellospring.controller;
 
+import Team2.youngcha.hellospring.domain.Income;
+import Team2.youngcha.hellospring.service.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -10,6 +15,17 @@ public class HomeController {
     public String home(){
         return "MainWeb";
     }
+    private final CustomerService customerService;
 
+    public HomeController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
+    @GetMapping("/Menu")
+    public String home(Model model){
+        List<Income> incomeList = customerService.getFavoriteMenu();
+        model.addAttribute("incomeList", incomeList);
+
+        return "Menu";
+    }
 }
